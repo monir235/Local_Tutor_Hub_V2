d0cbb878ba9f2840eb63038730810c63c771247d
Sirajum Monir <chayonayon71@gmail.com>
2025-09-27 02:38:44 +0600

    Modernized UI/UX for App.js and Home.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,7 @@ const Login = ({ onPageChange }) => {
     }
 
     // Make a POST request to your backend
-    const response = await fetch('http://localhost/loga.php', {
+    const response = await fetch('https://paloma-nonmicroscopic-marleigh.ngrok-free.app/loga.php', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
